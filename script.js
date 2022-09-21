69c8aa188a4ed8aae3a7c286683916fd4270b20a
tasmia-rafiq <tasmiarafiq14033@gmail.com>
2022-09-21 19:49:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
