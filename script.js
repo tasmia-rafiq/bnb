24494c8a7c795a6415eeab2540e4b6fca714f6c2
tasmia-rafiq <tasmiarafiq14033@gmail.com>
2022-09-23 19:11:39 +0500

    new3

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let nav = document.querySelector(".navbar");
+let searchToggler = document.querySelector(".search-toggler");
+let favorites = document.querySelector(".favorites");
 window.onscroll = function () {
-    if (document.documentElement.scrollTop > 20) {
+    if (document.documentElement.scrollTop > 150) {
         nav.classList.add("scroll-on");
+        searchToggler.classList.add("scroll-on");
+        favorites.classList.add("scroll-on");
     } else {
         nav.classList.remove("scroll-on");
+        searchToggler.classList.remove("scroll-on");
+        favorites.classList.remove("scroll-on");
     }
 }
